cc5ccafce919df48e1e366d46d800fad0cc21c81
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-03 20:27:38 +0200

    fix

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http')
-var port = 1337;
-//var port = process.env.PORT || 1337;
+var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Bro\n');
