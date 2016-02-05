351737336fa573d28f7c6e439ccda05f4bcc24c5
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-06 00:38:37 +0200

    admin panel update

--- admin/getEmail.php
+++ admin/getEmail.php
@@ -1,9 +1,9 @@
 <?php
   if(isset($_POST['text'])){
     if($mysqli = new mysqli("eu-cdbr-azure-north-d.cloudapp.net", "b2a32c755154bf", "c0b4e78d", "anspiritMain")){
-      /*if($mysqli->query("INSERT INTO email(from, to, subject, text) VALUES ($_POST['from'], $_POST['to'], $_POST['subject'], $_POST['text'])")){
+      if($mysqli->query("INSERT INTO email (from, to, subject, text) VALUES (\'$_POST['from']\', \'$_POST['to']\', \' $_POST['subject']\', \' $_POST['text']\')")){
         echo "Done, your email has been recieved!";
-      }*/
+      }
     }
   }
  ?>
