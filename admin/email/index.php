32aa4e218120a6172ba8ca5a1b3694d6c2dea29d
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-05 16:27:58 +0200

    email

--- admin/email/index.php
+++ admin/email/index.php
@@ -1,18 +1,75 @@
 <?php
+if(isset($_POST['send'])){
+  require 'PHPMailerAutoload.php';
 
-if (!defined('APP_VERSION'))
-{
-	define('APP_VERSION', '1.9.4.394');
-	define('APP_INDEX_ROOT_FILE', __FILE__);
-	define('APP_INDEX_ROOT_PATH', str_replace('\\', '/', rtrim(dirname(__FILE__), '\\/').'/'));
-}
+  $mail = new PHPMailer;
 
-if (file_exists(APP_INDEX_ROOT_PATH.'rainloop/v/'.APP_VERSION.'/include.php'))
-{
-	include APP_INDEX_ROOT_PATH.'rainloop/v/'.APP_VERSION.'/include.php';
+  $mail->isSMTP();                                        // Set mailer to use SMTP
+  $mail->Host = 'smtp.sendgrid.net';             // Specify main/backup SMTP servers
+  $mail->SMTPAuth = true;                           // Enable SMTP authentication
+  $mail->Username = 'azure_2eba721938d24ffa0fb3a2d9fde6cc43@azure.com';    // SMTP username
+  $mail->Password = 'Junction2015';    // SMTP password
+  $mail->SMTPSecure = 'tls';                        // Enable TLS/SSL encryption
+  $mail->Port = 587;                                      // TCP port to connect to
+
+  $mail->From = $_POST['from'];
+  $mail->FromName = $_POST['fromName'];
+  $mail->addAddress($_POST['to'], $_POST['toName']);     // Add a recipient
+
+  $mail->WordWrap = 50;                              // Set word wrap to 50 characters
+  $mail->isHTML(true);                                  // Set email format to HTML
+
+  $mail->Subject = $_POST['title'];
+  $mail->Body    = $_POST['emailContent'];
+
+  if(!$mail->send()) {
+      echo 'Message could not be sent.';
+      echo 'Mailer Error: ' . $mail->ErrorInfo;
+  } else {
+      echo 'Message has been sent';
+  }
 }
-else
-{
-	echo '[105] Missing version directory';
-	exit(105);
-}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Email</title>
+    <link rel="stylesheet" href="../admin.css" media="screen" charset="utf-8">
+  </head>
+  <body>
+    <?php if($_COOKIE['secret'] != 'Junction2015'){
+      echo "<h1> 404 </h1><br><h3>Page not found</h3>";
+    }else{
+      echo '
+          <h1>Email Client</h1>
+          <form method="post">
+          <table align="center">
+            <tr>
+              <td><h3>From: </h3></td><td><input type="email" name="from"></td>
+            </tr>
+            <tr>
+              <td><h3>From Name: </h3></td><td><input type="text" name="fromName"></td>
+            </tr>
+            <tr>
+              <td><h3>To: </h3></td><td><input type="email" name="to"></td>
+            </tr>
+            <tr>
+              <td><h3>To Name: </h3></td><td><input type="text" name="toName"></td>
+            </tr>
+            <tr>
+              <td><h3>Title: </h3></td><td><input type="text" name="title"></td>
+            </tr>
+            <tr>
+              <td><h3>Body: </h3></td><td><textarea name="emailContent" rows="20" cols="50"></textarea></td>
+            </tr>
+            <tr>
+              <td><input type="submit" name="send" value="Send"></td>
+            </tr>
+          </table>
+          </form>';
+    }
+    ?>
+  </body>
+</html>
--- admin/index.php
+++ admin/index.php
@@ -1,68 +1,20 @@
 <?php
-if(isset($_POST['send'])){
-  require 'PHPMailerAutoload.php';
 
-  $mail = new PHPMailer;
-
-  $mail->isSMTP();                                        // Set mailer to use SMTP
-  $mail->Host = 'smtp.sendgrid.net';             // Specify main/backup SMTP servers
-  $mail->SMTPAuth = true;                           // Enable SMTP authentication
-  $mail->Username = 'azure_2eba721938d24ffa0fb3a2d9fde6cc43@azure.com';    // SMTP username
-  $mail->Password = 'Junction2015';    // SMTP password
-  $mail->SMTPSecure = 'tls';                        // Enable TLS/SSL encryption
-  $mail->Port = 587;                                      // TCP port to connect to
-
-  $mail->From = $_POST['from'];
-  $mail->FromName = $_POST['fromName'];
-  $mail->addAddress($_POST['to'], $_POST['toName']);     // Add a recipient
-
-  $mail->WordWrap = 50;                              // Set word wrap to 50 characters
-  $mail->isHTML(true);                                  // Set email format to HTML
-
-  $mail->Subject = $_POST['title'];
-  $mail->Body    = $_POST['emailContent'];
-
-  if(!$mail->send()) {
-      echo 'Message could not be sent.';
-      echo 'Mailer Error: ' . $mail->ErrorInfo;
-  } else {
-      echo 'Message has been sent';
-  }
-}
 ?>
-
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Email</title>
+    <title>Admin CPanel</title>
+    <link rel="stylesheet" href="admin.css" media="screen" charset="utf-8">
   </head>
   <body>
-    <h1>Email Client</h1>
-    <form method="post">
-    <table align="center">
-      <tr>
-        <td><h3>From: </h3></td><td><input type="email" name="from"></td>
-      </tr>
-      <tr>
-        <td><h3>From Name: </h3></td><td><input type="text" name="fromName"></td>
-      </tr>
-      <tr>
-        <td><h3>To: </h3></td><td><input type="email" name="to"></td>
-      </tr>
-      <tr>
-        <td><h3>To Name: </h3></td><td><input type="text" name="toName"></td>
-      </tr>
-      <tr>
-        <td><h3>Title: </h3></td><td><input type="text" name="title"></td>
-      </tr>
-      <tr>
-        <td><h3>Body: </h3></td><td><textarea name="emailContent" rows="20" cols="50"></textarea></td>
-      </tr>
-      <tr>
-        <td><input type="submit" name="send" value="Send"></td>
-      </tr>
-    </table>
-    </form>
+    <?php
+      if($_COOKIE['secret'] == 'Junction2015'){
+        echo "<iframe src='adminMenu.html' width='200px' height='100%'></iframe>";
+      }else{
+        echo "<h1> 404 </h1><br><h3>Page not found</h3>";
+      }
+     ?>
   </body>
 </html>
