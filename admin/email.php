fe1b55c1c238229c02ac087ff7dbd1d113e11386
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-06 15:42:52 +0200

    fix

--- admin/email.php
+++ admin/email.php
@@ -1,53 +1,3 @@
-<?php
- include_once "swift/swift_required.php";
- if(isset($_POST['sendBtn'])){
-   $fromName = $_SESSION['username'] + " from Anspirit";
-   $fromEmail = $_SESSION['username'] + "@anspirit.org";
-   $toName = $_POST['toName'];
-   $toEmail = $_POST['toEmail'];
-   $mailHeader = $_POST['header'];
-   $mailContent = $_POST['content'];
-
-   $text = $mailContent;
-   // This is your From email address
-   $from = array($fromEmail => $fromName);
-   // Email recipients
-   $to = array(
-         $toEmail=>$toName
-   );
-   // Email subject
-   $subject = $mailHeader;
-
-   // Login credentials
-   $username = 'azure_2eba721938d24ffa0fb3a2d9fde6cc43@azure.com';
-   $password = 'Junction2015';
-
-   // Setup Swift mailer parameters
-   $transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
-   $transport->setUsername($username);
-   $transport->setPassword($password);
-   $swift = Swift_Mailer::newInstance($transport);
-
-   // Create a message (subject)
-   $message = new Swift_Message($subject);
-
-   // attach the body of the email
-   $message->setFrom($from);
-   $message->setTo($to);
-   $message->addPart($text, 'text/plain');
-
-   // send message
-   if ($recipients = $swift->send($message, $failures)){
-       echo 'Message has been sent';
-   }
-   // something went wrong =(
-   else
-   {
-       echo "Something went wrong - ";
-       print_r($failures);
-   }
- }
- ?>
  <!DOCTYPE html>
  <html>
    <head>
@@ -55,6 +5,56 @@
      <title>Anspirit email</title>
    </head>
    <body>
+     <?php
+      include_once "swift/swift_required.php";
+      if(isset($_POST['sendBtn'])){
+        $fromName = $_SESSION['username'] + " from Anspirit";
+        $fromEmail = $_SESSION['username'] + "@anspirit.org";
+        $toName = $_POST['toName'];
+        $toEmail = $_POST['toEmail'];
+        $mailHeader = $_POST['header'];
+        $mailContent = $_POST['content'];
+
+        $text = $mailContent;
+        // This is your From email address
+        $from = array($fromEmail => $fromName);
+        // Email recipients
+        $to = array(
+              $toEmail=>$toName
+        );
+        // Email subject
+        $subject = $mailHeader;
+
+        // Login credentials
+        $username = 'azure_2eba721938d24ffa0fb3a2d9fde6cc43@azure.com';
+        $password = 'Junction2015';
+
+        // Setup Swift mailer parameters
+        $transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
+        $transport->setUsername($username);
+        $transport->setPassword($password);
+        $swift = Swift_Mailer::newInstance($transport);
+
+        // Create a message (subject)
+        $message = new Swift_Message($subject);
+
+        // attach the body of the email
+        $message->setFrom($from);
+        $message->setTo($to);
+        $message->addPart($text, 'text/plain');
+
+        // send message
+        if ($recipients = $swift->send($message, $failures)){
+            echo '<h3>Message has been sent</h3>';
+        }
+        // something went wrong =(
+        else
+        {
+            echo "<h3>Something went wrong</h3>";
+            //print_r($failures);
+        }
+      }
+      ?>
       <!-- Mail UI -->
       <form method="post">
          <table>
