6146ac4a98c95a00910a12b6961791b384750616
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-06 16:21:08 +0200

    fix

--- admin/email.php
+++ admin/email.php
@@ -1,51 +1,4 @@
-<?php
-session_start();
-$fromName = $_SESSION['username'] + " from Anspirit";
-$fromEmail = $_SESSION['username'] + "@anspirit.org";
-$toName = 'Homo CEO';
-$toEmail = 'ansyli@gmail.com';
-$mailHeader = 'MOI';
-$mailContent = 'Hello my friend';
 
-$text = $mailContent;
-// This is your From email address
-$from = array($fromEmail => $fromName);
-// Email recipients
-$to = array(
-      $toEmail=>$toName
-);
-// Email subject
-$subject = $mailHeader;
-
-// Login credentials
-$username = 'azure_2eba721938d24ffa0fb3a2d9fde6cc43@azure.com';
-$password = 'Junction2015';
-
-// Setup Swift mailer parameters
-$transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
-$transport->setUsername($username);
-$transport->setPassword($password);
-$swift = Swift_Mailer::newInstance($transport);
-
-// Create a message (subject)
-$message = new Swift_Message($subject);
-
-// attach the body of the email
-$message->setFrom($from);
-$message->setTo($to);
-$message->addPart($text, 'text/plain');
-
-// send message
-if ($recipients = $swift->send($message, $failures)){
-    echo '<h3>Message has been sent</h3>';
-}
-// something went wrong =(
-else
-{
-    echo "<h3>Something went wrong</h3>";
-    //print_r($failures);
-}
- ?>
  <!DOCTYPE html>
  <html>
    <head>
@@ -54,6 +7,7 @@ else
    </head>
    <body>
      <?php
+      session_start();
       include_once "swift/swift_required.php";
       if(isset($_POST['sendBtn'])){
         $fromName = $_SESSION['username'] + " from Anspirit";
