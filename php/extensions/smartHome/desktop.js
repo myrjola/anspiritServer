6c509d6c266c2de510f9438a65e9343baefd5d44
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-10 14:30:54 +0200

    extension change

--- php/extensions/smartHome/desktop.js
+++ php/extensions/smartHome/desktop.js
@@ -1,11 +1,11 @@
 
 	exports.processSpeech = function(qapi, speech, callback){
-	    var toRet = {'done':false};
+	    var toRet = {done:false};
 	    callback(toRet);
 	}
 	exports.processActionFromSpeech = function(qapi, action, parameters, speech, emotion, callback){
 			 var qapi = require('../api/qapi.js');
-	     var toRet = {'done':false};
+	     var toRet = {done:false};
 			 if(action.contains('smarthome')){
 				 $.ajax({
 					 type: 'get',
@@ -14,7 +14,7 @@
 					 success: function(data){
 						 console.log("Sent request to hub!");
 						 console.log(data);
-						 toRet['done'] = true;
+						 toRet.done = true;
 						 callback(toRet);
 					 },
 					 error: function(a, error) {
