6ad563b11f4cedd495294bc5776a6a3650c93eed
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-10 11:26:56 +0200

    change

--- php/extensions/smartHome/desktop.js
+++ php/extensions/smartHome/desktop.js
@@ -1,12 +1,26 @@
 	exports.processSpeech = function(speech, callback){
-	     var toRet = {'done':false};
-	     //if done is false, rule search will be continued.
-      //if done is true, action must be performed, because rule search/executing will be done.
+	    var toRet = {'done':false};
 	    callback(toRet);
 	}
 	exports.processActionFromSpeech = function(action, parameters, speech, emotion, callback){
 	     var toRet = {'done':false};
-	     //if done is false, rule search will be continued.
-       //if done is true, action must be performed, because rule search/executing will be done.
-	    callback(toRet);
+			 if(action.contains('smarthome')){
+				 $.ajax({
+					 type: 'get',
+					 url: "http://api.anspirit.net:3030/hub/1",
+					 data: {task: {action: action, parameters: parameters}, secret: qapi.getUserSecret(), user: qapi.getUserId()},
+					 success: function(data){
+						 console.log("Sent request to hub!");
+						 console.log(data);
+						 toRet['done'] = true;
+						 callback(toRet);
+					 },
+					 error: function(a, error) {
+						 	callback(toRet);
+						 console.error(error);
+					 }
+				 });
+			 }else{
+				 	allback(toRet);
+			 }
 	}
