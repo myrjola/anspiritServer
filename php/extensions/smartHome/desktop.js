613b92f97520708fa269175b73497bdcf7bb92e0
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-11 21:49:59 +0200

    extension change

--- php/extensions/smartHome/desktop.js
+++ php/extensions/smartHome/desktop.js
@@ -12,7 +12,7 @@
 						 url: 'http://localhost:3000/hub',//"http://api.anspirit.net:3000/hub/1",
 						 data: {task: {action: action, parameters: parameters}, secret: global.qapi.getUserSecret(), user: global.qapi.getUserId(), hubId: 1},
 						 success: function(data){
-							 console.log("Data from hub: "data);
+							 console.log("Data from hub: " + data);
 							 toRet.done = true;
 							 global.qSay("Done", function(){});
 							 cb(toRet);
