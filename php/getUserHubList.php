975fc98b5761f404b175ce85ac42b45f1c8e1084
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-27 22:29:02 +0200

    get user hubs (update 2.0)

--- php/getUserHubList.php
+++ php/getUserHubList.php
@@ -2,15 +2,33 @@
 header("Content-Type: text/javascript");
 
 $returnArray = null;
-$id = $_GET['id'];
+$id = $_POST['id'];
+$password = $_POST['password'];
+
 $mysqli = new mysqli("eu-cdbr-azure-north-d.cloudapp.net", "b2a32c755154bf", "c0b4e78d", "anspiritMain");
 $query = "SELECT * FROM `users` WHERE `id`='".$id."'";
-  if($result = $mysqli -> query($query)){
+if($result = $mysqli -> query($query)){
     $row = $result -> fetch_assoc();
-    $returnArray = array('id'=>$id, 'email'=>$row['email'], 'hubList'=>$row['hubs']);
-  }else{
-    $returnArray = array('error'=>'error');
-  }
+    if($row['password'] == $password){
+        //$returnArray = array('id'=>$id, 'email'=>$row['email'], 'hubList'=>$row['hubs']);
+        $query = "SELECT * FROM `hub_list` WHERE `owner`='".$id."'";
+        if($result = $mysqli->query($query)){
+          $hubs = array();
+          while($row = $result->fetch_assoc()){
+            array_push($hubs, $row);
+          }
+          //Hubs are now in $hubs
+          $returnArray = $hubs;
+        }else{
+          $returnArray = array('error'=>'failed to contact database 2');
+        }
+    }else{
+        //No access granted
+        $returnArray = array('error'=>'no access');
+    }
+}else{
+    $returnArray = array('error'=>'failed to contact database 1');
+}
 
 echo json_encode($returnArray);
 
