586c7671cb9a64730d36c39fa66109579f62de57
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-06 10:43:22 +0200

    fix

--- php/extensions/googleSearch/desktop.js
+++ php/extensions/googleSearch/desktop.js
@@ -5,5 +5,31 @@
 	}
 	module.exports.processActionFromSpeech = function(action, parameters, speech, emotion, cb){
 	     var toRet = {done:false};
-			 cb(toRet);
+			 if(action != null){
+				 if(action.contains('smarthome')){
+					 if(window.NearestHub != null){
+							 $.ajax({
+								 type: 'get',
+								 url: "http://api.anspirit.net:3000/hub/1",
+								 data: {task: {action: action, parameters: parameters}, secret: global.qapi.getUserSecret(), user: global.qapi.getUserId(), hubId: window.NearestHub.id},
+								 success: function(data){
+									 console.log("Data from hub: " + data);
+									 toRet.done = true;
+									 global.qSay("Done", function(){});
+									 cb(toRet);
+								 },
+								 error: function(a, error) {
+									 cb(toRet);
+									 console.error(error);
+								 }
+							 });
+						 }else {
+							 console.log("No nearest hub found!");
+						 }
+				 }else{
+						cb(toRet);
+				 }
+			 }else{
+				 cb(toRet);
+			 }
 	}
---
+++ php/extensions/soundcloud/desktop.js
@@ -0,0 +1,9 @@
+
+	module.exports.processSpeech = function(speech, cb){
+	    var toRet = {done:false};
+	    cb(toRet);
+	}
+	module.exports.processActionFromSpeech = function(action, parameters, speech, emotion, cb){
+	     var toRet = {done:false};
+			 cb(toRet);
+	}
