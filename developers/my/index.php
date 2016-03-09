3624f3b1007f2d24bbd122740ef46615bc2feb8e
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-09 22:45:23 +0200

    update

--- developers/my/index.php
+++ developers/my/index.php
@@ -104,7 +104,7 @@ body {
     <h1>Login</h1>
     <input type="text" value="" placeholder="Username" id="username" />
     <input type="password" value="" placeholder="Password" id="password" />
-    <button>Loginn</button>
+    <button>Login</button>
 </div>
 </body>
 
