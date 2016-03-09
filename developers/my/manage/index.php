bcad3dfced920d78e456b478ba96a821ac4d949c
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-09 23:42:11 +0200

    update

---
+++ developers/my/manage/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Anspirit - Manage Developer</title>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <meta name="description" content="IOT is our future. Everyday people create more and more smart devices. These devices are irreplaceable part of our lives. But how we use them? Every smart device means new app on your smartphone, desktop, smartwatch edc. Anspirit and qproject will connect all of them with each other using one simple ecosystem based on Rest API. Every device can control each other and get information from them.">
+    <meta property="og:description" content="IOT is our future. Everyday people create more and more smart devices. These devices are irreplaceable part of our lives. But how we use them? Every smart device means new app on your smartphone, desktop, smartwatch edc. Anspirit and qproject will connect all of them with each other using one simple ecosystem based on Rest API. Every device can control each other and get information from them.">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../images/anspirit.ico">
+    <link rel="apple-touch-icon" href="../../../images/anspirit.ico">
+    <title>Anspirit - Developer login</title>
+    <style media="screen">
+    body {
+        background: url('http://i.imgur.com/Eor57Ae.jpg') no-repeat fixed center center;
+        background-size: cover;
+        font-family: Montserrat;
+    }
+    footer{
+      position: absolute;
+      bottom: 100px;
+      left: 100px;
+    }
+    </style>
+  </head>
+  <body>
+
+    <footer>
+      <img src="../../../images/anspirit.ico" width="100px"/>
+    </footer>
+  </body>
+</html>
