59855ebd107467a64badded5356284b4a52e7fc3
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-12 14:53:44 +0200

    update

--- developers/my/manage/edit.php
+++ developers/my/manage/edit.php
@@ -8,6 +8,26 @@
   <head>
     <meta charset="utf-8">
     <title>Edit</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <style media="screen">
+      input .field{
+        width: 100%;
+        height: 30px;
+        font-size: 20px;
+        border-radius: 0px;
+        background-color: rgb(200, 200, 200);
+        color: rgb(40, 40, 40);
+      }
+      input .submit{
+        height: 30px;
+        font-size: 20px;
+        border-radius: 0px;
+        background-color: rgb(4, 184, 0);
+        color: rgb(40, 40, 40);
+        width: 200px;
+        margin-left: calc(100% - 100px);
+      }
+    </style>
     <script type="text/javascript">
       //Update input field values with real data
     </script>
@@ -15,11 +35,15 @@
   <body>
     <h1>Edit extension</h1>
     <form class="" action="/php/editExtension.php" method="post">
-      <input type="text" name="name">
+      <input type="text" name="name" class="field"><br>
+
+      <!-- Hidden inputs -->
       <input type="hidden" name="devId" value="developer id">
       <input type="hidden" name="devPassword" value="developer password">
       <input type="hidden" name="extensionId" value="extension id">
-      <input type="submit" value="Update">
+      <!-- Hidden inputs end -->
+
+      <input type="submit" value="Update" class="submit">
     </form>
   </body>
 </html>
