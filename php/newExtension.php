ae68f84b06f2fc7e1d9b7d8e0833daac2b41e8b2
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-12 23:44:59 +0200

    update

--- php/newExtension.php
+++ php/newExtension.php
@@ -1,3 +1,34 @@
 <?php
+  $name = $_POST['name'];
+  $price = $_POST['price'];
+  $description = $_POST['description'];
+  $pathToExt = $_POST['pathToExt'];
+  $icon = $_POST['icon']
+  $devId = $_POST['devId'];
+  $devPassword = $_POST['devPassword'];
 
- ?>
+  $mysqli = new mysqli("eu-cdbr-azure-north-d.cloudapp.net", "b2a32c755154bf", "c0b4e78d", "anspiritMain");
+  $query = "SELECT * FROM `developers` WHERE `devId`='". $devId ."'";
+  if($result = $mysqli -> query($query)){
+    if($row = $result -> fetch_assoc()){
+      //Check access
+      if($row['password'] == $devPassword){
+        //Granted!
+        $developer = $row['name'];
+        $query = "INSERT INTO extensions` (`name`, `description`, `developer`, `icon`, `pathToExt`, `price`, `soldTimes`) VALUES ('".$name."', '".$description."', '".$developer."', '".$icon."', '".$pathToExt."', '0', '0')";
+        if ($mysqli -> query($query)) {
+          //Done
+          header("Location: http://anspirit.org/developers/my/manage");
+        }else{
+          //Failed execute query
+          echo $query;
+        }
+      }else{
+        //Login failed
+      }
+    }else{
+      //No developer found
+      echo "No developer found";
+    }
+  }
+?>
