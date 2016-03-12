14fde59881913171ba8c771cf1a9b911a4f00a5f
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-12 10:31:48 +0200

    update

--- php/developersExtensions.php
+++ php/developersExtensions.php
@@ -1,6 +1,5 @@
 <?php
-//SELECT * FROM `extensions` WHERE `dev_id` = 100;
-if(isset($_POST['id'])){
+ if(isset($_POST['id'])){
   //Got request
   $developer = $_POST['id'];
   $mysqli = new mysqli("eu-cdbr-azure-north-d.cloudapp.net", "b2a32c755154bf", "c0b4e78d", "anspiritMain");
