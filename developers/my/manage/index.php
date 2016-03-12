969c6a876035e540a4d7fc1fea8ac809c178de4d
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-12 10:34:58 +0200

    update

--- developers/my/manage/index.php
+++ developers/my/manage/index.php
@@ -36,7 +36,6 @@
     <title>Anspirit - Manage Developer</title>
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
     <meta name="description" content="IOT is our future. Everyday people create more and more smart devices. These devices are irreplaceable part of our lives. But how we use them? Every smart device means new app on your smartphone, desktop, smartwatch edc. Anspirit and qproject will connect all of them with each other using one simple ecosystem based on Rest API. Every device can control each other and get information from them.">
-    <meta property="og:description" content="IOT is our future. Everyday people create more and more smart devices. These devices are irreplaceable part of our lives. But how we use them? Every smart device means new app on your smartphone, desktop, smartwatch edc. Anspirit and qproject will connect all of them with each other using one simple ecosystem based on Rest API. Every device can control each other and get information from them.">
     <link rel="shortcut icon" type="image/x-icon" href="../../../images/anspirit.ico">
     <link rel="apple-touch-icon" href="../../../images/anspirit.ico">
     <title>Anspirit - Developer login</title>
