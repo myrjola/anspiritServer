05c71fa0416b8193f51aa43dbc059a91526336a3
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-16 18:04:38 +0200

    up

--- php/getUserHubList.php
+++ php/getUserHubList.php
@@ -12,7 +12,6 @@ if($result = $mysqli -> query($query)){
     if($row['password'] == md5($password)){
         //$returnArray = array('id'=>$id, 'email'=>$row['email'], 'hubList'=>$row['hubs']);
         $query = "SELECT * FROM `hub_list` WHERE `ownerId`='".$id."'";
-        echo $query;
         if($result = $mysqli->query($query)){
           $hubs = array('hubList'=>array());
           while($row = $result->fetch_assoc()){
