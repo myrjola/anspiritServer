1decbdacf2a87ba297c0327d51de8ed3b1b63061
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-18 22:50:29 +0200

    update soundcloud

--- php/extensions/soundcloud/desktop.js
+++ php/extensions/soundcloud/desktop.js
@@ -8,9 +8,9 @@ module.exports.processSpeech = function(speech, callback) {
     global.qapi.loadScript("http://anspirit.org/php/extensions/soundcloud/scSearch.js", function() {
       global.qapi.loadScript("http://anspirit.org/php/extensions/soundcloud/scPlayer.js", function(){
         toRet.done = true;
-        scSearch("Avicii", 2, function callback(tracks){
+        scSearch("Avicii", 10, function callback(tracks){
           for(var i = 0; i < tracks.length; i++){
-              console.log(tracks[i].genre);
+              console.log(tracks[i].name);
           }
           callback(toRet);
         });
