037f61d47dd3d48102283590560769163b7d99d2
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-03-20 13:08:52 +0200

    new

--- php/extensions/soundcloud/desktop.js
+++ php/extensions/soundcloud/desktop.js
@@ -20,8 +20,8 @@ module.exports.processActionFromSpeech = function(action, parameters, speech, em
             var url = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/34019569&color=0066cc";
             embded = embded + updateURLParameter(url, 'url', song);
             embded = embded + '"></iframe>';
-            newCard(embded);
             callback(toRet);
+            newCard(embded);
       });
     });
   }else{
